c76b9df499556871174e94ca646c6ca61686d421
James Touton <bekenn@gmail.com>
2016-01-10 00:12:07 -0800

    Added conversion functions between utf8 and utf16.

--- include/stdext/unicode.h
+++ include/stdext/unicode.h
@@ -21,6 +21,19 @@ namespace stdext
         error
     };
 
+    struct utfstate_t
+    {
+        union
+        {
+            ::std::uint32_t value;
+            ::std::uint32_t
+                code : 24,
+                produced : 2,
+                consumed : 2,
+                remaining : 2;
+        };
+    };
+
     constexpr char32_t UNICODE_REPLACEMENT_CHARACTER = 0xFFFD;
     constexpr char32_t INVALID_UNICODE_CHARACTER = char32_t(-1);
     constexpr ::std::size_t MAX_UTF8_CHARACTER_LENGTH = 4;
@@ -63,6 +76,74 @@ namespace stdext
         return (code & 0xFC00) == 0xDC00;
     }
 
+    namespace detail
+    {
+        bool utf8_decode_first(::std::uint8_t code, utfstate_t& state)
+        {
+            if ((code & 0x80) == 0x00)
+            {
+                state.code = code;
+                state.remaining = 0;
+            }
+            else if ((code & 0xE0) == 0xC0)
+            {
+                state.code = code & 0x1F;
+                state.remaining = 1;
+            }
+            else if ((code & 0xF0) == 0xE0)
+            {
+                state.code = code & 0x0F;
+                state.remaining = 2;
+            }
+            else if ((code & 0xF8) == 0xF0)
+            {
+                state.code = code & 0x07;
+                state.remaining = 3;
+            }
+            else
+                return false;
+
+            return true;
+        }
+
+        bool utf8_validate_second(::std::uint8_t code, utfstate_t state)
+        {
+            switch (state.remaining)
+            {
+            case 2:
+                if ((state.code == 0x00 && code < 0xA0)
+                    || (state.code == 0x0D && code >= 0xA0))
+                {
+                    return false;
+                }
+                break;
+            case 3:
+                if ((state.code == 0x00 && code < 0x90)
+                    || (state.code == 0x04 && code >= 0x90))
+                {
+                    return false;
+                }
+                break;
+
+            default:
+                break;
+            }
+
+            return true;
+        }
+
+        bool utf8_decode_trailing(::std::uint8_t code, utfstate_t& state)
+        {
+            if ((code & 0xC0) != 0x80)
+                return false;
+
+            state.code <<= 6;
+            state.code |= code;
+            --state.remaining;
+            return true;
+        }
+    }
+
     // Converts as many code units as possible from utf8 to utf32.  This function supports partial conversions, such that
     // all utf8 code units are consumed until either the input buffer has been completely consumed or the output buffer
     // has been completely filled.  If the input buffer ends with a partial character sequence, no corresponding output
@@ -79,301 +160,259 @@ namespace stdext
     // If the function returns utf_result::error, state == 0 and in_first points one position beyond the input character
     // sequence that could not be converted.
     // The behavior is undefined if state != 0 or if state does not contain a value from a previous call to this function.
-    utf_result utf8_to_utf32(const char*& in_first, const char* in_last, char32_t*& out_first, char32_t* out_last, uint32_t& state)
+    utf_result utf8_to_utf32(const char*& in_first, const char* in_last, char32_t*& out_first, char32_t* out_last, utfstate_t& state)
     {
-        constexpr unsigned REMAINING_SHIFT = 24;
-        constexpr unsigned CONSUMED_SHIFT = 28;
-
         if (in_first == in_last)
-            return state == 0 ? utf_result::ok : utf_result::partial;
+            return state.consumed == 0 ? utf_result::ok : utf_result::partial;
 
-        char32_t utf32 = state & 0x00FFFFFF;
-        unsigned remaining = (state >> REMAINING_SHIFT) & 0x0F;
-        unsigned consumed = (state >> CONSUMED_SHIFT) & 0x0F;
+        state.code;
+        state.remaining;
+        state.consumed;
 
-        switch (consumed)
+        switch (state.consumed)
         {
             while (in_first != in_last)
             {
         case 0:
+                if (!detail::utf8_decode_first(*in_first++, state))
                 {
-                    auto code = ::std::uint8_t(*in_first++);
-                    consumed = 1;
-
-                    if ((code & 0x80) == 0)
-                    {
-                        utf32 = code;
-                        remaining = 0;
-                    }
-                    else if ((code & 0xE0) == 0xC0)
-                    {
-                        utf32 = code & 0x1F;
-                        remaining = 1;
-                    }
-                    else if ((code & 0xF0) == 0xE0)
-                    {
-                        utf32 = code & 0x0F;
-                        remaining = 2;
-                    }
-                    else if ((code & 0xF8) == 0xF0)
-                    {
-                        utf32 = code & 0x07;
-                        remaining = 3;
-                    }
-                    else
-                    {
-                        state = 0;
-                        return utf_result::error;
-                    }
-
-                    if (in_first == in_last)
-                    {
-                        state = (consumed << CONSUMED_SHIFT)
-                            | (remaining << REMAINING_SHIFT)
-                            | utf32;
-                        return utf_result::partial;
-                    }
+                    state.value = 0;
+                    return utf_result::error;
                 }
+
+                state.consumed = 1;
+                if (in_first == in_last)
+                    return utf_result::partial;
 
         // fall through
         case 1:
-                switch (remaining)
+                if (!detail::utf8_validate_second(*in_first, state))
                 {
-                case 2:
-                    if ((utf32 == 0x00 && *in_first < 0xA0)
-                        || (utf32 == 0x0D && *in_first >= 0xA0))
-                    {
-                        state = 0;
-                        return utf_result::error;
-                    }
-                    break;
-                case 3:
-                    if ((utf32 == 0x00 && *in_first < 0x90)
-                        || (utf32 == 0x04 && *in_first >= 0x90))
-                    {
-                        state = 0;
-                        return utf_result::error;
-                    }
-                    break;
-
-                default:
-                    break;
+                    state.value = 0;
+                    return utf_result::error;
                 }
 
         // fall through
         default:
-                while (remaining != 0)
+                while (state.remaining != 0)
                 {
                     if (in_first == in_last)
-                    {
-                        state = (consumed << CONSUMED_SHIFT)
-                            | (remaining << REMAINING_SHIFT)
-                            | utf32;
                         return utf_result::partial;
-                    }
 
-                    auto code = *in_first;
-                    if ((code & 0xC0) != 0x80)
+                    if (!detail::utf8_decode_trailing(*in_first, state))
                     {
-                        state = 0;
+                        state.value = 0;
                         return utf_result::error;
                     }
 
                     ++in_first;
-                    ++consumed;
-                    --remaining;
-
-                    utf32 <<= 6;
-                    utf32 |= code & 0x3F;
+                    ++state.consumed;
                 }
 
                 if (out_first == out_last)
-                {
-                    state = (consumed << CONSUMED_SHIFT)
-                        | (remaining << REMAINING_SHIFT)
-                        | utf32;
                     return utf_result::partial;
-                }
-                *out_first++ = utf32;
+
+                *out_first++ = state.code;
             }
         }
 
-        state = 0;
+        state.value = 0;
         return utf_result::ok;
     }
 
-    utf_result utf16_to_utf32(const char16_t*& in_first, const char16_t* in_last, char32_t*& out_first, char32_t* out_last, uint32_t& state)
+    utf_result utf16_to_utf32(const char16_t*& in_first, const char16_t* in_last, char32_t*& out_first, char32_t* out_last, utfstate_t& state)
     {
-        constexpr unsigned CONSUMED_SHIFT = 24;
-
         if (in_first == in_last)
-            return state == 0 ? utf_result::ok : utf_result::partial;
+            return state.consumed == 0 ? utf_result::ok : utf_result::partial;
 
-        char32_t utf32 = state;
-        unsigned consumed = utf32 >> CONSUMED_SHIFT;
-
-        switch (consumed)
+        switch (state.consumed)
         {
             while (in_first != in_last)
             {
         case 0:
-                utf32 = *in_first++;
-                consumed = 1;
+                state.code = *in_first++;
+                state.consumed = 1;
 
-                if (is_leading_surrogate(char16_t(utf32)))
+                if (is_leading_surrogate(char16_t(state.code)))
                 {
-                    utf32 &= 0x03FF;
-                    utf32 += 0x40;
+                    state.code &= 0x03FF;
+                    state.code += 0x40;
 
                     if (in_first == in_last)
-                    {
-                        state = utf32;
                         return utf_result::partial;
-                    }
 
         // fall through
         case 1:
                     auto code = *in_first;
                     if (!is_trailing_surrogate(code))
                     {
-                        state = 0;
+                        state.value = 0;
                         return utf_result::error;
                     }
                     ++in_first;
-                    ++consumed;
+                    ++state.consumed;
 
-                    utf32 <<= 10;
-                    utf32 |= code & 0x03FF;
+                    state.code <<= 10;
+                    state.code |= code & 0x03FF;
                 }
 
         // fall through
         default:
                 if (out_first == out_last)
-                {
-                    state = utf32
-                        | (consumed << CONSUMED_SHIFT);
                     return utf_result::partial;
-                }
 
-                *out_first++ = utf32;
+                *out_first++ = state.code;
             }
         }
 
-        state = 0;
+        state.value = 0;
         return utf_result::ok;
     }
 
-    utf_result utf32_to_utf8(const char32_t*& in_first, const char32_t* in_last, char*& out_first, char* out_last, uint32_t& state)
+    utf_result utf32_to_utf8(const char32_t*& in_first, const char32_t* in_last, char*& out_first, char* out_last, utfstate_t& state)
     {
-        constexpr unsigned REMAINING_SHIFT = 24;
-        constexpr unsigned PRODUCED_SHIFT = 28;
-
-        char32_t code = state & 0x00FFFFFF;
-        unsigned remaining = (state >> REMAINING_SHIFT) & 0x0F;
-        unsigned produced = (state >> PRODUCED_SHIFT) & 0x0F;
-
-        switch (produced)
+        switch (state.produced)
         {
         case 0:
             while (in_first != in_last)
             {
-                code = *in_first++;
-                if (code >= 0x110000 || (code < 0x10000 && is_surrogate(char16_t(code))))
+                state.code = *in_first++;
+                if (state.code >= 0x110000 || (state.code < 0x10000 && is_surrogate(char16_t(state.code))))
                 {
-                    state = 0;
+                    state.value = 0;
                     return utf_result::error;
                 }
 
-                remaining = code < 0x80 ? 0
-                    : code < 0x800 ? 1
-                    : code < 0x1000 ? 2
+                state.remaining = state.code < 0x80 ? 0
+                    : state.code < 0x800 ? 1
+                    : state.code < 0x1000 ? 2
                     : 3;
 
-                switch (remaining)
+                switch (state.remaining)
                 {
                 case 0:
-                    *out_first++ = char(code);
+                    *out_first++ = char(state.code);
                     break;
                 case 1:
-                    *out_first++ = char(0xC0 | (code >> 6));
+                    *out_first++ = char(0xC0 | (state.code >> 6));
                     break;
                 case 2:
-                    *out_first++ = char(0xE0 | (code >> 12));
+                    *out_first++ = char(0xE0 | (state.code >> 12));
                     break;
                 case 3:
-                    *out_first++ = char(0xF0 | (code >> 18));
+                    *out_first++ = char(0xF0 | (state.code >> 18));
                     break;
                 }
-                produced = 1;
+                state.produced = 1;
 
-                while (remaining != 0)
+                while (state.remaining != 0)
                 {
         default:
                     if (out_first == out_last)
-                    {
-                        state = (produced << PRODUCED_SHIFT)
-                            | (remaining << REMAINING_SHIFT)
-                            | code;
                         return utf_result::partial;
-                    }
 
-                    --remaining;
-                    *out_first++ = char(0x80 | ((code >> 6 * remaining) & 0x3F));
+                    --state.remaining;
+                    *out_first++ = char(0x80 | ((state.code >> 6 * state.remaining) & 0x3F));
                 }
             }
         }
 
-        state = 0;
+        state.value = 0;
         return utf_result::ok;
     }
 
-    utf_result utf32_to_utf16(const char32_t*& in_first, const char32_t* in_last, char16_t*& out_first, char16_t* out_last, uint32_t& state)
+    utf_result utf32_to_utf16(const char32_t*& in_first, const char32_t* in_last, char16_t*& out_first, char16_t* out_last, utfstate_t& state)
     {
-        constexpr unsigned PRODUCED_SHIFT = 24;
-
-        char32_t code = state & 0x00FFFFFF;
-        unsigned produced = state >> PRODUCED_SHIFT;
-
-        switch (produced)
+        switch (state.produced)
         {
         case 0:
             while (in_first != in_last)
             {
-                code = *in_first++;
-                if (code >= 0x110000 || (code < 0x10000 && is_surrogate(char16_t(code))))
+                state.code = *in_first++;
+                if (state.code >= 0x110000 || (state.code < 0x10000 && is_surrogate(char16_t(state.code))))
                 {
-                    state = 0;
+                    state.value = 0;
                     return utf_result::error;
                 }
 
                 if (out_first == out_last)
-                {
-                    state = (produced << PRODUCED_SHIFT)
-                        | code;
                     return utf_result::partial;
-                }
 
-                if (code < 0x10000)
-                    *out_first++ = char16_t(code);
+                if (state.code < 0x10000)
+                    *out_first++ = char16_t(state.code);
                 else
                 {
-                    *out_first++ = char16_t(0xD800 | ((code >> 16) - 1));
-                    produced = 1;
+                    *out_first++ = char16_t(0xD800 | ((state.code >> 16) - 1));
+                    state.produced = 1;
 
         default:
                     if (out_first == out_last)
-                    {
-                        state = (produced << PRODUCED_SHIFT)
-                            | code;
                         return utf_result::partial;
-                    }
 
-                    *out_first++ = char16_t(0xDC00 | (code & 0xFFFF));
+                    *out_first++ = char16_t(0xDC00 | (state.code & 0xFFFF));
                 }
             }
         }
 
-        state = 0;
+        state.value = 0;
+        return utf_result::ok;
+    }
+
+    utf_result utf8_to_utf16(const char*& in_first, const char* in_last, char16_t*& out_first, char16_t* out_last, utfstate_t& state)
+    {
+        char32_t code;
+        char32_t* pcode;
+        const char32_t* pccode;
+        utf_result result;
+
+        switch (state.produced)
+        {
+        case 0:
+            while (in_first != in_last)
+            {
+                pcode = &code;
+                result = utf8_to_utf32(in_first, in_last, pcode, pcode + 1, state);
+                if (result != utf_result::ok)
+                    return result;
+
+        default:
+            pccode = &code;
+            result = utf32_to_utf16(pccode, pccode + 1, out_first, out_last, state);
+            if (result != utf_result::ok)
+                return result;
+            }
+        }
+
+        state.value = 0;
+        return utf_result::ok;
+    }
+
+    utf_result utf16_to_utf8(const char16_t*& in_first, const char16_t* in_last, char*& out_first, char* out_last, utfstate_t& state)
+    {
+        char32_t code;
+        char32_t* pcode;
+        const char32_t* pccode;
+        utf_result result;
+
+        switch (state.produced)
+        {
+        case 0:
+            while (in_first != in_last)
+            {
+                pcode = &code;
+                result = utf16_to_utf32(in_first, in_last, pcode, pcode + 1, state);
+                if (result != utf_result::ok)
+                    return result;
+
+        default:
+                pccode = &code;
+                result = utf32_to_utf8(pccode, pccode + 1, out_first, out_last, state);
+                if (result != utf_result::ok)
+                    return result;
+            }
+        }
+
+        state.value = 0;
         return utf_result::ok;
     }
 
@@ -439,14 +478,14 @@ namespace stdext
                 break;
             }
 
-            uint32_t state = 0;
+            utfstate_t state = 0;
             auto result = utf_result::error;
             do
             {
                 char utf8 = wrapped.at_pos(pos);
                 wrapped.inc_pos(pos);
 
-                auto putf8 = &utf8;
+                const char* putf8 = &utf8;
                 auto putf32 = &value;
 
                 result = utf8_to_utf32(putf8, putf8 + 1, putf32, putf32 + 1, state);
