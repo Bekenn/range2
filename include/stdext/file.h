b40a5920845af494718505463e51311094470f99
James Touton <bekenn@gmail.com>
2016-05-15 19:55:13 -0700

    Implemented file_output_stream on Windows.

--- include/stdext/file.h
+++ include/stdext/file.h
@@ -11,40 +11,66 @@
 #pragma once
 
 #include "stream.h"
+#include "types.h"
 
 
 namespace stdext
 {
 #ifdef _WIN32
     using path_char = wchar_t;
+    using file_handle_t = void*;
 #define PATH_STR(s) L ## s
 #else
     using pach_char = char;
+    using file_handle_t = int;
 #define PATH_STR(s) s
 #endif
 
-    class file_input_stream : public input_stream, public seekable
+    enum class file_open_flags
     {
-    public:
-#ifdef _WIN32
-        using native_handle_t = void*;
-#else
-        using native_handle_t = int;
-#endif
+        none = 0,
+        create = 1,
+        truncate = 2
+    };
 
+    class file_input_stream;
+    class file_output_stream;
+
+    namespace detail
+    {
+        class file_stream_base : public seekable
+        {
+        public:
+            explicit file_stream_base(file_handle_t handle);
+            ~file_stream_base() override;
+
+        public:
+            void seek(seek_from from, ::std::ptrdiff_t offset) override;
+
+        protected:
+            file_handle_t handle;
+        };
+    }
+
+    class file_input_stream : public detail::file_stream_base, public input_stream
+    {
     public:
         explicit file_input_stream(const path_char* path);
         ~file_input_stream() override;
 
-    public:
-        void seek(seek_from from, ::std::ptrdiff_t offset) override;
-
     private:
         ::std::size_t do_read(void* buffer, ::std::size_t size) override;
         ::std::size_t do_skip(::std::size_t size) override;
+    };
+
+    class file_output_stream : public detail::file_stream_base, public output_stream
+    {
+    public:
+        file_output_stream(const path_char* path, flags<file_open_flags> flags);
+        ~file_output_stream() override;
 
     private:
-        native_handle_t handle;
+        virtual ::std::size_t do_write(const void* buffer, ::std::size_t size) override;
     };
 }
 
--- src/windows/file.cpp
+++ src/windows/file.cpp
@@ -8,42 +8,56 @@
 
 namespace stdext
 {
-    file_input_stream::file_input_stream(const path_char* path)
-        : handle(::CreateFile(path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, nullptr, OPEN_EXISTING, 0, nullptr))
+    namespace
     {
-        if (handle == INVALID_HANDLE_VALUE)
-            throw std::system_error(::GetLastError(), std::system_category());
+        DWORD creation_disposition(flags<file_open_flags> flags);
     }
 
-    file_input_stream::~file_input_stream()
+    namespace detail
     {
-        ::CloseHandle(handle);
-    }
-
-    void file_input_stream::seek(seek_from from, std::ptrdiff_t offset)
-    {
-        LARGE_INTEGER distance;
-        distance.QuadPart = offset;
-
-        DWORD method;
-        switch (from)
+        file_stream_base::file_stream_base(file_handle_t handle) : handle(handle)
         {
-        case seek_from::begin:
-            method = FILE_BEGIN;
-            break;
-
-        case seek_from::current:
-            method = FILE_CURRENT;
-            break;
-
-        case seek_from::end:
-            method = FILE_END;
-            break;
+            if (handle == INVALID_HANDLE_VALUE)
+                throw std::system_error(::GetLastError(), std::system_category());
         }
 
-        if (!::SetFilePointerEx(handle, distance, nullptr, method))
-            throw std::system_error(::GetLastError(), std::system_category());
+        file_stream_base::~file_stream_base()
+        {
+            ::CloseHandle(handle);
+        }
+
+        void file_stream_base::seek(seek_from from, std::ptrdiff_t offset)
+        {
+            LARGE_INTEGER distance;
+            distance.QuadPart = offset;
+
+            DWORD method;
+            switch (from)
+            {
+            case seek_from::begin:
+                method = FILE_BEGIN;
+                break;
+
+            case seek_from::current:
+                method = FILE_CURRENT;
+                break;
+
+            case seek_from::end:
+                method = FILE_END;
+                break;
+            }
+
+            if (!::SetFilePointerEx(handle, distance, nullptr, method))
+                throw std::system_error(::GetLastError(), std::system_category());
+        }
     }
+
+    file_input_stream::file_input_stream(const path_char* path)
+        : file_stream_base(::CreateFile(path, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_DELETE, nullptr, OPEN_EXISTING, 0, nullptr))
+    {
+    }
+
+    file_input_stream::~file_input_stream() = default;
 
     std::size_t file_input_stream::do_read(void* buffer, std::size_t size)
     {
@@ -53,15 +67,15 @@ namespace stdext
         std::size_t bytes = 0;
         while (size != 0)
         {
-            DWORD count = size > MAXDWORD ? MAXDWORD & ~(granularity - 1) : size;
-            DWORD dwbytes;
-            if (!::ReadFile(handle, p, count, &dwbytes, nullptr))
+            DWORD chunk_size = size > MAXDWORD ? MAXDWORD & ~(granularity - 1) : size;
+            DWORD chunk_bytes;
+            if (!::ReadFile(handle, p, chunk_size, &chunk_bytes, nullptr))
                 throw std::system_error(::GetLastError(), std::system_category());
-            if (dwbytes == 0)
+            if (chunk_bytes == 0)
                 break;
-            size -= dwbytes;
-            bytes += dwbytes;
-            p += dwbytes;
+            size -= chunk_bytes;
+            bytes += chunk_bytes;
+            p += chunk_bytes;
         }
 
         return bytes;
@@ -77,10 +91,59 @@ namespace stdext
         if (!::SetFilePointerEx(handle, LARGE_INTEGER(), &distance, FILE_CURRENT))
             throw std::system_error(::GetLastError(), std::system_category());
 
-        distance.QuadPart = ::std::min(static_cast<LONGLONG>(size), file_size.QuadPart - distance.QuadPart);
+        distance.QuadPart = std::min(static_cast<LONGLONG>(size), file_size.QuadPart - distance.QuadPart);
         if (!::SetFilePointerEx(handle, distance, nullptr, FILE_CURRENT))
             throw std::system_error(::GetLastError(), std::system_category());
 
         return static_cast<std::size_t>(distance.QuadPart);
+    }
+
+    file_output_stream::file_output_stream(const path_char* path, flags<file_open_flags> flags)
+        : file_stream_base(::CreateFile(path, GENERIC_WRITE, 0, nullptr, creation_disposition(flags), FILE_ATTRIBUTE_NORMAL, nullptr))
+    {
+    }
+
+    file_output_stream::~file_output_stream() = default;
+
+    std::size_t file_output_stream::do_write(const void* buffer, std::size_t size)
+    {
+        constexpr DWORD granularity = 0x1000;
+
+        auto p = static_cast<const uint8_t*>(buffer);
+        std::size_t bytes = 0;
+        while (size != 0)
+        {
+            DWORD chunk_size = size > MAXDWORD ? MAXDWORD & ~(granularity - 1) : size;
+            DWORD chunk_bytes;
+            if (!::WriteFile(handle, p, chunk_size, &chunk_bytes, nullptr))
+                throw std::system_error(::GetLastError(), std::system_category());
+            if (chunk_bytes < chunk_size)
+                return bytes + chunk_bytes;
+            size -= chunk_bytes;
+            bytes += chunk_bytes;
+            p += chunk_bytes;
+        }
+
+        return bytes;
+    }
+
+    namespace
+    {
+        DWORD creation_disposition(flags<file_open_flags> flags)
+        {
+            switch (flags)
+            {
+            case file_open_flags::none:
+                return OPEN_EXISTING;
+            case file_open_flags::create:
+                return OPEN_ALWAYS;
+            case file_open_flags::truncate:
+                return TRUNCATE_EXISTING;
+            case stdext::flags<file_open_flags>(file_open_flags::create, file_open_flags::truncate):
+                return CREATE_ALWAYS;
+            }
+
+            return -1;
+        }
     }
 }
