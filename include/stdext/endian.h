17afb09196f04a8edc1efdd9598059a6002058bd
James Touton <bekenn@gmail.com>
2018-01-09 01:26:19 -0800

    Endian operations customized solely on swap.

--- include/stdext/endian.h
+++ include/stdext/endian.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "traits.h"
+#include "stream.h"
 
-#ifdef STDEXT_ARCH_X86
+#if STDEXT_ARCH_X86 && !STDEXT_COMPILER_GCC
 #include <immintrin.h>
 #endif
 
@@ -35,24 +35,67 @@ namespace stdext
     }
 
     template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
-    T read(const void* p)
-    {
-        using sized_t = equivalent_sized_type_t<T>;
-        return detail::endian<order, sized_t>::read(p);
-    }
-
-    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
-    void write(void* p, T v)
-    {
-        using sized_t = equivalent_sized_type_t<T>;
-        return detail::endian<order, sized_t>::write(p, sized_t(v));
-    }
-
-    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
     T swap(T v)
     {
+        if constexpr (order == byte_order::native_endian)
+            return v;
+
         using sized_t = equivalent_sized_type_t<T>;
-        return detail::endian<order, sized_t>::swap(sized_t(v));
+        return T(detail::endian<order, sized_t>::swap(sized_t(v)));
+    }
+
+    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
+    T read(input_stream& s)
+    {
+        using sized_t = equivalent_sized_type_t<T>;
+        return T(swap<order>(s.read<sized_t>()));
+    }
+
+    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
+    size_t read(input_stream& s, T* buffer, size_t count)
+    {
+        count = s.read(buffer, count);
+        if constexpr (order != byte_order::native_endian)
+        {
+            for (size_t n = 0; n < count; ++n)
+                buffer[n] = swap<order>(buffer[n]);
+        }
+        return count;
+    }
+
+    template <byte_order order, class T, size_t length, REQUIRES(::std::is_integral_v<T>)>
+    size_t read(input_stream& s, T (&buffer)[length])
+    {
+        using sized_t = equivalent_sized_type_t<T>;
+        return detail::endian<order, sized_t>::read(s, buffer, length);
+    }
+
+    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
+    void write(output_stream& s, T v)
+    {
+        s.write(swap<order>(v));
+    }
+
+    template <byte_order order, class T, REQUIRES(::std::is_integral_v<T>)>
+    size_t write(output_stream& s, const T* buffer, size_t count)
+    {
+        if constexpr (order == byte_order::native_endian)
+            return s.write(buffer, count);
+
+        size_t n = 0;
+        for (; n < count; ++n)
+        {
+            auto v = swap<order>(buffer[n]);
+            if (s.write(&v, 1) == 0)
+                break;
+        }
+        return n;
+    }
+
+    template <byte_order order, class T, size_t length, REQUIRES(::std::is_integral_v<T>)>
+    size_t write(output_stream& s, const T (&buffer)[length])
+    {
+        return write(s, buffer, length);
     }
 
     namespace detail
@@ -60,16 +103,6 @@ namespace stdext
         template <byte_order order, class T>
         struct endian
         {
-            static T read(const void* p)
-            {
-                return endian<order, ::std::make_unsigned_t<T>>::read(p);
-            }
-
-            static void write(void* p, T v)
-            {
-                endian<order, ::std::make_unsigned_t<T>>::write(p, v);
-            }
-
             static T swap(T v)
             {
                 return endian<order, ::std::make_unsigned_t<T>>::swap(v);
@@ -79,16 +112,6 @@ namespace stdext
         template <class T>
         struct endian<byte_order::native_endian, T>
         {
-            static T read(const void* p)
-            {
-                return *static_cast<const T*>(p);
-            }
-
-            static void write(void* p, T v)
-            {
-                *static_cast<T*>(p) = v;
-            }
-
             static T swap(T v)
             {
                 return v;
@@ -98,16 +121,6 @@ namespace stdext
         template <byte_order order>
         struct endian<order, uint8_t>
         {
-            static uint8_t read(const void* p)
-            {
-                return *static_cast<const uint8_t*>(p);
-            }
-
-            static void write(void* p, uint8_t v)
-            {
-                *static_cast<uint8_t*>(p) = v;
-            }
-
             static uint8_t swap(uint8_t v)
             {
                 return v;
@@ -117,16 +130,6 @@ namespace stdext
         template <>
         struct endian<byte_order::native_endian, uint8_t>
         {
-            static uint8_t read(const void* p)
-            {
-                return *static_cast<const uint8_t*>(p);
-            }
-
-            static void write(void* p, uint8_t v)
-            {
-                *static_cast<uint8_t*>(p) = v;
-            }
-
             static uint8_t swap(uint8_t v)
             {
                 return v;
@@ -137,24 +140,6 @@ namespace stdext
         template <>
         struct endian<byte_order::little_endian, uint16_t>
         {
-            static uint16_t read(const void* p)
-            {
-#if STDEXT_COMPILER_GCC
-                return __builtin_bswap16(*static_cast<const uint16_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint16_t v)
-            {
-#if STDEXT_COMPILER_GCC
-                *static_cast<uint16_t*>(p) = __builtin_bswap16(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint16_t swap(uint16_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -168,24 +153,6 @@ namespace stdext
         template <>
         struct endian<byte_order::little_endian, uint32_t>
         {
-            static uint32_t read(const void* p)
-            {
-#if STDEXT_COMPILER_GCC
-                return __builtin_bswap32(*static_cast<const uint32_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint32_t v)
-            {
-#if STDEXT_COMPILER_GCC
-                *static_cast<uint32_t*>(p) = __builtin_bswap32(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint32_t swap(uint32_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -199,24 +166,6 @@ namespace stdext
         template <>
         struct endian<byte_order::little_endian, uint64_t>
         {
-            static uint64_t read(const void* p)
-            {
-#if STDEXT_COMPILER_GCC
-                return __builtin_bswap32(*static_cast<const uint64_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint64_t v)
-            {
-#if STDEXT_COMPILER_GCC
-                *static_cast<uint64_t*>(p) = __builtin_bswap64(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint64_t swap(uint64_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -232,28 +181,6 @@ namespace stdext
         template <>
         struct endian<byte_order::big_endian, uint16_t>
         {
-            static uint16_t read(const void* p)
-            {
-#if STDEXT_ARCH_X86
-                return _load_be_u16(p);
-#elif STDEXT_COMPILER_GCC
-                return __builtin_bswap16(*static_cast<const uint16_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint16_t v)
-            {
-#if STDEXT_ARCH_X86
-                _store_be_u16(p, v);
-#elif STDEXT_COMPILER_GCC
-                *static_cast<uint16_t*>(p) = __builtin_bswap16(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint16_t swap(uint16_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -269,28 +196,6 @@ namespace stdext
         template <>
         struct endian<byte_order::big_endian, uint32_t>
         {
-            static uint32_t read(const void* p)
-            {
-#ifdef STDEXT_ARCH_X86
-                return _load_be_u32(p);
-#elif STDEXT_COMPILER_GCC
-                return __builtin_bswap32(*static_cast<const uint32_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint32_t v)
-            {
-#if STDEXT_ARCH_X86
-                _store_be_u32(p, v);
-#elif STDEXT_COMPILER_GCC
-                *static_cast<uint32_t*>(p) = __builtin_bswap32(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint32_t swap(uint32_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -306,28 +211,6 @@ namespace stdext
         template <>
         struct endian<byte_order::big_endian, uint64_t>
         {
-            static uint64_t read(const void* p)
-            {
-#ifdef STDEXT_ARCH_X86
-                return _load_be_u64(p);
-#elif STDEXT_COMPILER_GCC
-                return __builtin_bswap64(*static_cast<const uint64_t*>(p));
-#else
-#error Implement me
-#endif
-            }
-
-            static void write(void* p, uint64_t v)
-            {
-#if STDEXT_ARCH_X86
-                _store_be_u64(p, v);
-#elif STDEXT_COMPILER_GCC
-                *static_cast<uint64_t*>(p) = __builtin_bswap64(v);
-#else
-#error Implement me
-#endif
-            }
-
             static uint64_t swap(uint64_t v)
             {
 #if STDEXT_COMPILER_GCC
@@ -345,16 +228,6 @@ namespace stdext
         template <>
         struct endian<byte_order::pdp_endian, uint16_t>
         {
-            uint16_t read(const void* p)
-            {
-                return endian<byte_order::little_endian, uint16_t>::read(p);
-            }
-
-            static void write(void* p, uint16_t v)
-            {
-                endian<byte_order::little_endian, uint16_t>::write(p, v);
-            }
-
             static uint16_t swap(uint16_t v)
             {
                 return endian<byte_order::little_endian, uint16_t>::swap(v);
@@ -364,20 +237,6 @@ namespace stdext
         template <>
         struct endian<byte_order::pdp_endian, uint32_t>
         {
-            static uint32_t read(const void* p)
-            {
-                auto pb = static_cast<const uint8_t*>(p);
-                return (uint32_t(endian<byte_order::little_endian, uint16_t>::read(pb)) << 16)
-                    | endian<byte_order::little_endian, uint16_t>::read(pb + 2);
-            }
-
-            static void write(void* p, uint32_t v)
-            {
-                auto pb = static_cast<uint8_t*>(p);
-                endian<byte_order::little_endian, uint16_t>::write(pb, uint16_t(v >> 16));
-                endian<byte_order::little_endian, uint16_t>::write(pb + 2, uint16_t(v));
-            }
-
             static uint32_t swap(uint32_t v)
             {
 #if STDEXT_LITTLE_ENDIAN
@@ -395,20 +254,6 @@ namespace stdext
         template <>
         struct endian<byte_order::pdp_endian, uint64_t>
         {
-            static uint64_t read(const void* p)
-            {
-                auto pb = static_cast<const uint8_t*>(p);
-                return (uint64_t(endian<byte_order::pdp_endian, uint32_t>::read(pb)) << 32)
-                    | endian<byte_order::pdp_endian, uint32_t>::read(pb + 4);
-            }
-
-            static void write(void* p, uint64_t v)
-            {
-                auto pb = static_cast<uint8_t*>(p);
-                endian<byte_order::pdp_endian, uint32_t>::write(pb, uint32_t(v >> 32));
-                endian<byte_order::pdp_endian, uint32_t>::write(pb + 4, uint32_t(v));
-            }
-
             static uint64_t swap(uint64_t v)
             {
 #if STDEXT_LITTLE_ENDIAN
