41070df04b48a6d61c94a92066d33fa6d39cdd74
James Touton <bekenn@gmail.com>
2018-07-22 01:27:21 -0700

    Don't need these definitions anymore (yay inline variables!)

--- src/file.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdext/file.h>
-
-
-namespace stdext
-{
-    constexpr flags<file_open_flags> file_output_stream::default_flags;
-    constexpr flags<file_open_flags> file_stream::default_flags;
-}
