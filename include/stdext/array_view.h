d3896760ffb9d39cfa970e876093669a2e8f65f3
James Touton <bekenn@gmail.com>
2018-07-25 03:33:04 -0700

    Added construction and assignment from arrays.

--- include/stdext/array_view.h
+++ include/stdext/array_view.h
@@ -31,9 +31,17 @@ namespace stdext
 
     public:
         array_view() noexcept = default;
+        template <class U, size_t size,
+            REQUIRES(std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<U>>
+            && std::is_convertible_v<U*, T*>)>
+        array_view(U (&a)[size]) noexcept : delimited_iterator_range<T*>(a, a + size) { }
         array_view(pointer p, size_type size) noexcept : delimited_iterator_range<T*>(p, p + size) { }
 
     public:
+        template <class U, size_t size,
+            REQUIRES(std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<U>>
+            && std::is_convertible_v<U*, T*>)>
+        array_view& operator = (U (&a)[size]) { begin_pos(a); end_pos(a + size); return *this; }
         auto data() const noexcept { return this->begin(); }
         bool empty() const noexcept { return this->begin() == this->end(); }
 
@@ -56,9 +64,17 @@ namespace stdext
 
     public:
         const_array_view() noexcept = default;
+        template <class U, size_t size,
+            REQUIRES(std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<U>>
+            && std::is_convertible_v<U*, const T*>)>
+        const_array_view(U(&a)[size]) noexcept : delimited_iterator_range<const T*>(a, a + size) { }
         const_array_view(pointer p, size_type size) noexcept : delimited_iterator_range<const T*>(p, p + size) { }
 
     public:
+        template <class U, size_t size,
+            REQUIRES(std::is_same_v<std::remove_cv_t<T>, std::remove_cv_t<U>>
+            && std::is_convertible_v<U*, const T*>)>
+        const_array_view& operator = (U(&a)[size]) { begin_pos(a); end_pos(a + size); return *this; }
         auto data() const noexcept { return this->begin(); }
         bool empty() const noexcept { return this->begin() == this->end(); }
 
