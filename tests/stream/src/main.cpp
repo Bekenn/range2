e18b90a1a0b24586d6c3f9736583ae28d5fa303e
James Touton <bekenn@gmail.com>
2021-11-23 13:51:36 -0800

    Integrated Catch2 into stream test project.

--- tests/stream/src/main.cpp
+++ tests/stream/src/main.cpp
@@ -8,72 +8,64 @@
 
 #include <stdext/stream.h>
 
-#undef NDEBUG
-#include <cassert>
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
 
-using namespace std;
-using namespace stdext;
 
-template <class T, size_t length>
-constexpr size_t lengthof(T (&)[length])
+const std::byte stuff[] =
 {
-    return length;
-}
-
-const byte stuff[] =
-{
-    byte(0), byte(1), byte(2), byte(3), byte(4), byte(5), byte(6), byte(7),
-    byte(8), byte(9), byte(0xa), byte(0xb), byte(0xc), byte(0xd), byte(0xe), byte(0xf)
+    std::byte(0), std::byte(1), std::byte(2), std::byte(3), std::byte(4), std::byte(5), std::byte(6), std::byte(7),
+    std::byte(8), std::byte(9), std::byte(0xa), std::byte(0xb), std::byte(0xc), std::byte(0xd), std::byte(0xe), std::byte(0xf)
 };
 
 template <class POD, size_t size>
 void test(const POD (&data)[size])
 {
-    memory_input_stream is(stuff, sizeof(stuff));
+    stdext::memory_input_stream is(stuff, sizeof(stuff));
     for (auto n : data)
-        assert(n == is.read<POD>());
+        REQUIRE(n == is.read<POD>());
 
     is.reset(stuff, sizeof(stuff));
     for (auto n : data)
-        assert(n == is.read<POD>());
+        REQUIRE(n == is.read<POD>());
 
-    is.seek(seek_from::begin, 0);
+    is.seek(stdext::seek_from::begin, 0);
     for (auto n : data)
-        assert(n == is.read<POD>());
+        REQUIRE(n == is.read<POD>());
 
-    is.seek(seek_from::begin, 0);
-    input_stream_iterator<POD> i(is);
+    is.seek(stdext::seek_from::begin, 0);
+    stdext::input_stream_iterator<POD> i(is);
     for (auto n : data)
-        assert(n == *i++);
-    assert(i == input_stream_iterator<POD>());
+        REQUIRE(n == *i++);
+    REQUIRE(i == stdext::input_stream_iterator<POD>());
 
-    is.seek(seek_from::begin, 0);
-    i = input_stream_iterator<POD>(is);
+    is.seek(stdext::seek_from::begin, 0);
+    i = stdext::input_stream_iterator<POD>(is);
     for (auto n : data)
-        assert(n == *i++);
-    assert(i == input_stream_iterator<POD>());
+        REQUIRE(n == *i++);
+    REQUIRE(i == stdext::input_stream_iterator<POD>());
 
-    is.seek(seek_from::begin, 0);
-    i = input_stream_iterator<POD>(is);
+    is.seek(stdext::seek_from::begin, 0);
+    i = stdext::input_stream_iterator<POD>(is);
     for (auto n : data)
     {
-        assert(n == *i);
+        REQUIRE(n == *i);
         ++i;
     }
-    assert(i == input_stream_iterator<POD>());
+    REQUIRE(i == stdext::input_stream_iterator<POD>());
 }
 
-int main()
+TEST_CASE("Stream operations", "[stream]")
 {
-    static const uint8_t data8[] =
+    static const std::uint8_t data8[] =
     {
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf
     };
-    static const uint16_t data16[] =
+    static const std::uint16_t data16[] =
     {
         0x0100, 0x0302, 0x0504, 0x0706, 0x0908, 0x0b0a, 0x0d0c, 0x0f0e
     };
-    static const uint32_t data32[] =
+    static const std::uint32_t data32[] =
     {
         0x03020100, 0x07060504, 0x0b0a0908, 0x0f0e0d0c
     };
