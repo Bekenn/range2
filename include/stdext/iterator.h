e99b0319e8fd1fd66d07368e6005a74064753747
James Touton <bekenn@gmail.com>
2022-01-01 21:58:30 -0800

    Added tests for iterator.

--- include/stdext/iterator.h
+++ include/stdext/iterator.h
@@ -26,33 +26,35 @@ namespace stdext
         STDEXT_DECLARE_HAS_INNER_TYPE(iterator_category);
     };
     template <typename Iterator> struct is_iterator
-        : std::conditional_t<_private::STDEXT_HAS_INNER_TYPE_V(std::iterator_traits<Iterator>, iterator_category),
-            true_type,
-            false_type>
+        : std::bool_constant<_private::STDEXT_HAS_INNER_TYPE_V(std::iterator_traits<remove_cvref_t<Iterator>>, iterator_category)>
     { };
+    template <> struct is_iterator<void*> : false_type { };
+    template <> struct is_iterator<const void*> : false_type { };
+    template <> struct is_iterator<volatile void*> : false_type { };
+    template <> struct is_iterator<const volatile void*> : false_type { };
 
-    template <typename Iterator> using iterator_category = typename std::iterator_traits<Iterator>::iterator_category;
+    template <typename Iterator> using iterator_category = typename std::iterator_traits<remove_cvref_t<Iterator>>::iterator_category;
     namespace _private
     {
         template <typename Iterator>
         struct value_type_of<Iterator, is_iterator, true>
         {
-            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<Iterator>::value_type>;
+            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<remove_cvref_t<Iterator>>::value_type>;
         };
         template <typename Iterator>
         struct difference_type_of<Iterator, is_iterator, true>
         {
-            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<Iterator>::difference_type>;
+            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<remove_cvref_t<Iterator>>::difference_type>;
         };
         template <typename Iterator>
         struct pointer_type_of<Iterator, is_iterator, true>
         {
-            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<Iterator>::pointer>;
+            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<remove_cvref_t<Iterator>>::pointer>;
         };
         template <typename Iterator>
         struct reference_type_of<Iterator, is_iterator, true>
         {
-            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<Iterator>::reference>;
+            using type = std::enable_if_t<is_iterator<Iterator>::value, typename std::iterator_traits<remove_cvref_t<Iterator>>::reference>;
         };
         template <typename Iterator>
         struct size_type_of<Iterator, is_iterator, true>
@@ -72,23 +74,27 @@ namespace stdext
     template <typename Iterator> using iterator_size_type = size_type<Iterator, is_iterator>;
     template <typename Iterator> using iterator_size_type_t = size_type_t<Iterator, is_iterator>;
 
-    template <typename Iterator> struct is_input_iterator
-    {
-        static constexpr bool value = std::is_base_of<std::input_iterator_tag, iterator_category<Iterator>>::value;
-    };
-    template <typename Iterator> struct is_forward_iterator
-    {
-        static constexpr bool value = std::is_base_of<std::forward_iterator_tag, iterator_category<Iterator>>::value;
-    };
-    template <typename Iterator> struct is_bidirectional_iterator
-    {
-        static constexpr bool value = std::is_base_of<std::bidirectional_iterator_tag, iterator_category<Iterator>>::value;
-    };
-    template <typename Iterator> struct is_random_access_iterator
-    {
-        static constexpr bool value = std::is_base_of<std::random_access_iterator_tag, iterator_category<Iterator>>::value;
-    };
+    template <typename Iterator, bool IsIterator = is_iterator<Iterator>::value>
+    struct is_input_iterator : std::is_base_of<std::input_iterator_tag, iterator_category<Iterator>> { };
+    template <typename Iterator>
+    struct is_input_iterator<Iterator, false> : false_type { };
 
+    template <typename Iterator, bool IsIterator = is_iterator<Iterator>::value>
+    struct is_forward_iterator : std::is_base_of<std::forward_iterator_tag, iterator_category<Iterator>> { };
+    template <typename Iterator>
+    struct is_forward_iterator<Iterator, false> : false_type { };
+
+    template <typename Iterator, bool IsIterator = is_iterator<Iterator>::value>
+    struct is_bidirectional_iterator : std::is_base_of<std::bidirectional_iterator_tag, iterator_category<Iterator>> { };
+    template <typename Iterator>
+    struct is_bidirectional_iterator<Iterator, false> : false_type { };
+
+    template <typename Iterator, bool IsIterator = is_iterator<Iterator>::value>
+    struct is_random_access_iterator : std::is_base_of<std::random_access_iterator_tag, iterator_category<Iterator>> { };
+    template <typename Iterator>
+    struct is_random_access_iterator<Iterator, false> : false_type { };
+
+    template <typename Iterator> constexpr auto is_iterator_v = is_iterator<Iterator>::value;
     template <typename Iterator> constexpr auto is_input_iterator_v = is_input_iterator<Iterator>::value;
     template <typename Iterator> constexpr auto is_forward_iterator_v = is_forward_iterator<Iterator>::value;
     template <typename Iterator> constexpr auto is_bidirectional_iterator_v = is_bidirectional_iterator<Iterator>::value;
@@ -101,14 +107,14 @@ namespace stdext
         using value_type = typename std::iterator_traits<Iterator>::value_type;
 
     public:
-        explicit iterator_proxy(const value_type& value) : value(value) { }
-        explicit iterator_proxy(value_type&& value) : value(stdext::move(value)) { }
+        iterator_proxy(const value_type& value) : _value(value) { }
+        iterator_proxy(value_type&& value) : _value(stdext::move(value)) { }
 
     public:
-        value_type operator * () { return stdext::move(value); }
+        value_type&& operator * () && { return stdext::move(_value); }
 
     private:
-        value_type value;
+        value_type _value;
     };
 }
 
