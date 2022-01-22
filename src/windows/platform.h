98dd806bc5c4c7a5ff8bbfe64764057be1f0c8fe
James Touton <bekenn@gmail.com>
2022-01-22 02:06:08 -0800

    Enable conformant preprocessor when building with MSVC.

--- src/windows/platform.h
+++ src/windows/platform.h
@@ -2,7 +2,16 @@
 #define STDEXT_IMPL_PLATFORM_INCLUDED
 #pragma once
 
+#if defined _MSVC_TRADITIONAL && !_MSVC_TRADITIONAL
+#pragma warning(push)
+#pragma warning(disable: 5105)  // macro expansion producing 'defined' has undefined behavior
+#endif
+
 #define NOMINMAX
 #include <Windows.h>
+
+#if defined _MSVC_TRADITIONAL && !_MSVC_TRADITIONAL
+#pragma warning(pop)
+#endif
 
 #endif
